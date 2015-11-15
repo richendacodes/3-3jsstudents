6028ac7c04d59b4672188b682a4729757db152d4
Richenda Gould <Richenda Gould>
2015-11-15 18:04:02 -0500

    Create new row

--- js/students.js
+++ js/students.js
@@ -10,14 +10,39 @@ for ( i = 0 ; i < 6; i++) {
 }
 
 
-// var makeStudent = document.getElementById('btnMake')
 
-// function myCreateFunction() {
-//     var table = document.getElementById("myTable");
-//     var row = table.insertRow(0);
-//     var cell1 = row.insertCell(0);
-//     var cell2 = row.insertCell(1);
-//     cell1.innerHTML = "NEW CELL1";
-//     cell2.innerHTML = "NEW CELL2";
+// Works to create a new row with one blank input field. ===========================
+
+// function addRow() {
+
+//     var tableID="studentTable";
+//     var table = document.getElementById('studentTable');
+//     var rowCount = table.rows.length;
+
+//     if(rowCount<10){
+//         var row = table.insertRow(rowCount);
+//         var cell1 = row.insertCell(0);
+//         var element1 = document.createElement('input');
+//         element1.type="text";
+//         cell1.appendChild(element1);
+//         row.insertCell(1);
+//         row.insertCell(2);
+//     }
 // }
+
+
+// This works, but doesn't fill the fields dynamically. 
+
+function addRow() {
+    var table = document.getElementById("studentTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "Mona";
+    cell2.innerHTML = "Simpson";
+    cell3.innerHTML = "Email";
+    cell4.innerHTML = "No X";
+}
 
